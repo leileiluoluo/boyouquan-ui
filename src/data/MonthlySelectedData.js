d0a751a5eb2ed7833f7762b3a6f06a8d649ef21c
Jane <Jane@larrys-MacBook-Pro.local>
2024-10-13 13:24:29 +0800

    page: monthly selected

---
+++ src/data/MonthlySelectedData.js
@@ -0,0 +1,14 @@
+export const monthlySelectedData = [
+    {
+        name: '2024/09 精选文章',
+        items: [
+            {
+                blogName: 'Macin的博客',
+                blogDomainURL: '/blogs/macin.org',
+                postName: '贵阳筑城',
+                postURL: '/go?ssss',
+                publishedAt: '2024/09/24'
+            }
+        ]
+    }
+]

4b231684db8c85429b58b41954f3a23818e4a8ed
Jane <Jane@larrys-MacBook-Pro.local>
2024-10-14 10:33:13 +0800

    page: monthly selected

--- src/pages/MonthlySelected.js
+++ src/pages/MonthlySelected.js
@@ -1,7 +1,6 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import Header from '../components/Header'
 import MainContentHeader from '../components/MainContentHeader';
-import { monthlySelectedData } from '../data/MonthlySelectedData';
 import FooterBlock from '../components/FooterBlock';
 import SiteFooter from '../components/SiteFooter';
 import FooterJS from '../components/FooterJS';
@@ -9,9 +8,23 @@ import HeaderJS from '../components/HeaderJS';
 import GoToTop from '../components/GoToTop';
 
 export default function MonthlySelected() {
+    const [posts, setPosts] = useState([]);
+
     useEffect(() => {
         document.title = '每月精选 - 博友圈 · 博客人的朋友圈！';
+
+        fetch('https://www.boyouquan.com/api/monthly-selected/all')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setPosts(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
     }, []);
+
+    console.log('posts:' + JSON.stringify(posts));
 
     const tableStyle = { display: 'table', tableLayout: 'fixed' };
     const dateStyle = { marginRight: '6px' };
@@ -24,53 +37,50 @@ export default function MonthlySelected() {
                 <MainContentHeader />
 
                 <div className="monthly-selected-container">
-                    <div className="monthly-selected-single">
-                        <div className="blog-detail-articles">
-                            {
-                                monthlySelectedData.map(
-                                    (item, index) => (
-                                        <Fragment>
-                                            <div className="articles-title">
-                                                <h4 key={`Outter-${index}`}>{item.name}</h4>
-                                            </div>
-                                            <div className="articles-container">
-                                                <table style={tableStyle}>
-                                                    <thead>
-                                                        <tr>
-                                                            <td width="20%"><span>博客名称</span></td>
-                                                            <td width="60%"><span>文章标题</span></td>
-                                                            <td width="20%"><span>发布时间</span></td>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        {
-                                                            item.items.map(
-                                                                (itemt, indext) => {
-                                                                    return (
-                                                                        <tr key={`Inner-${indext}`}>
-                                                                            <td width="20%">
-                                                                                <a href={itemt.blogDomainURL}>{itemt.blogName}</a>
-                                                                            </td>
-                                                                            <td width="60%">
-                                                                                <a href={itemt.postURL} target="_blank">{itemt.postName}</a>
-                                                                            </td>
-                                                                            <td width="20%">
-                                                                                <p style={dateStyle}>{itemt.publishedAt}</p>
-                                                                            </td>
-                                                                        </tr>
-                                                                    )
-                                                                }
-                                                            )
-                                                        }
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </Fragment>
-                                    )
-                                )
-                            }
-                        </div>
-                    </div>
+                    {
+                        posts.map(
+                            (item, index) => (
+                                <div className="monthly-selected-single">
+                                    <div className="blog-detail-articles">
+                                        <div className="articles-title">
+                                            <h4 key={`Outter-${index}`}>{item.yearMonthStr}</h4>
+                                        </div>
+                                        <div className="articles-container">
+                                            <table style={tableStyle}>
+                                                <thead>
+                                                    <tr>
+                                                        <td width="20%"><span>博客名称</span></td>
+                                                        <td width="60%"><span>文章标题</span></td>
+                                                        <td width="20%"><span>发布时间</span></td>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {
+                                                        item.postInfos.map(
+                                                            (itemt, indext) => {
+                                                                return (
+                                                                    <tr key={`Inner-${indext}`}>
+                                                                        <td width="20%">
+                                                                            <a href={`https://www.boyouquan.com/blogs/${itemt.blogDomainName}`}>{itemt.blogName}</a>
+                                                                        </td>
+                                                                        <td width="60%">
+                                                                            <a href={`https://www.boyouquan.com/go?link=${itemt.link}`} target="_blank">{itemt.title}</a>
+                                                                        </td>
+                                                                        <td width="20%">
+                                                                            <p style={dateStyle}>{itemt.publishedAt}</p>
+                                                                        </td>
+                                                                    </tr>
+                                                                )
+                                                            }
+                                                        )
+                                                    }
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                    }
                 </div>
                 {/* <footer className="page-footer blog-footer">
                     <nav className="pagination">

6bea8aea6e8786fa55878b454c48c12bd24c91f2
Jane <Jane@larrys-MacBook-Pro.local>
2024-10-29 11:37:51 +0800

    refactoring pages

---
+++ src/pages/GoPage.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import getURLParameter from '../utils/CommonUtil';
+import Meta from '../components/common/Meta';
+import RequestUtil from '../utils/RequestUtil';
+
+const meta = {
+    title: '网址跳转 - 博友圈 · 博客人的朋友圈！',
+    keywords: '网址跳转, 博友圈',
+    description: '博友圈网址跳转。'
+};
+
+export default function GoPage() {
+    const from = getURLParameter('from') || '';
+    const link = getURLParameter('link') || '';
+
+    const fetchData = async (link, from) => {
+        const resp = await RequestUtil.get(`https://www.boyouquan.com/api/go?from=${from}&link=${link}`);
+        window.location.href = resp.link;
+    };
+
+    useEffect(() => {
+        fetchData(link, from);
+    }, [link, from]);
+
+    return (
+        <Meta meta={meta} />
+    )
+}

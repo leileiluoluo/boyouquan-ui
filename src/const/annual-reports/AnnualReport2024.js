16d1ed8bc1db5144204be3e1e1d6782bdf431dfd
Jane <Jane@larrys-MacBook-Pro.local>
2024-12-09 16:19:16 +0800

    annual reports

--- src/const/annual-reports/AnnualReport2024.js
+++ src/const/annual-reports/AnnualReport2024.js
@@ -11,7 +11,7 @@ const REPORT_INFO_2024 = {
 
         <h3>1 年度数字</h3>
         <p>2024 全年，博友圈共收录了 <strong>392</strong> 个博客，其中 <strong>203</strong> 个为自行提交，<strong>189</strong> 个为后台收录。</p>
-        <p><img border="1" src="/assets/images/sites/annual_reports/2024/blog-collection-pie-chart.svg" /></p>
+        <p><img border="1" src="/assets/images/sites/annual_reports/2024/blog-collection-pie-chart.png" /></p>
 
         <p>2024 全年，大家共发布了 <strong>15909</strong> 篇文章，其中 <strong>1180</strong> 篇被加入到了首页推荐，<strong>61</strong> 篇被置顶过。发布文章最多的博客为「<a href="https://www.boyouquan.com/blogs/rushihu.com">如是乎 · 生活百味随笔</a>」，共发布了 <strong>316</strong> 篇文章。</p>
         <p>2024 全年，博友圈共给大家带去了 <strong>537971</strong> 个独立 IP 访问，其中访问量最高的博客为「<a href="https://www.boyouquan.com/blogs/rushihu.com">如是乎 · 生活百味随笔</a>」，共获得了 <strong>6877</strong> 个独立 IP 访问。访问量最高的文章为「<a href="https://www.boyouquan.com/sharing?link=https%3A%2F%2Fwww.xiangshitan.com%2Fpost%2F773.html">空空如也</a>」，共获得了 <strong>869</strong> 个独立 IP 访问。</p>

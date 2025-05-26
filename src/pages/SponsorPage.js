bbb41775d778b51bdabe841dea458038df034970
Jane <Jane@larrys-MacBook-Pro.local>
2025-05-26 11:48:12 +0800

    sponsor

--- src/pages/SponsorPage.js
+++ src/pages/SponsorPage.js
@@ -524,6 +524,20 @@ const content = (
                         </p>
                     </td>
                 </tr>
+                <tr>
+                    <td>
+                        <a href="/blogs/zhoutian.com">周天记</a>
+                    </td>
+                    <td>2025/05/26</td>
+                    <td>9.00 元</td>
+                    <td>支持站长，让服务器多运行一会～</td>
+                    <td>已计入总额，待后续使用</td>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>
+                        <p>当前总额：541.49 元</p>
+                    </td>
+                </tr>
             </tbody>
         </table>
 

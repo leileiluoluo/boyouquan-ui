d6f88e2b71557bbaee9d718bad8f74bafff23a27
leileiluoluo <leileiluoluo@leileiluoluo.com>
2025-06-05 11:40:19 +0800

    Update SpecialThanks.js

--- src/components/common/special-thanks/SpecialThanks.js
+++ src/components/common/special-thanks/SpecialThanks.js
@@ -131,10 +131,6 @@ export default function SpecialThanks() {
                         <span className="tooltiptext">库拉饭的博客</span>
                     </div>
                     <div className="blogger-one">
-                        <a href="/blogs/vrast.cn"><img src="https://www.boyouquan.com/gravatar/daf8e241a023b5c47ea90d1041c51413?size=80" /></a>
-                        <span className="tooltiptext">Keyle's Blog</span>
-                    </div>
-                    <div className="blogger-one">
                         <a href="/sponsor"><img src="/assets/images/sites/special_thanks/plus.svg" /></a>
                         <span className="tooltiptext">我也要赞助</span>
                     </div>

f82528a102b1bfeaba3915094cb4bcac7152e05b
Jane <Jane@larrys-MacBook-Pro.local>
2025-07-28 21:38:04 +0800

    radix ui: latest news

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -44,7 +44,7 @@ export default function HomePage() {
                         <Flex direction="column" gap="4">
                             <MainContentHeader content='您有多久没有读过一篇长文了？那些记忆中有趣的博客还在更新吗？博友圈是博客人的专属朋友圈，连接还在写博的博友，让那属于文字的时代延续光辉！' />
                             <HomePopularBlogsHeader />
-                            {/* <HomeLatestNews /> */}
+                            <HomeLatestNews />
                             <SearchBox placeholder='搜索文章 ↵' gotoPage='/home' sortType='latest' />
                             <SwitchSortType types={switchTypes} />
                             <PostCardList sort={sort} keyword={keyword} showPinned={showPinned} />

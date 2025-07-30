4a47cffc03384cb56d268cf7b7c8645d4d26d6a2
Jane <Jane@larrys-MacBook-Pro.local>
2025-07-30 09:53:44 +0800

    abstract

--- src/components/abstract/AbstractGo.js
+++ src/components/abstract/AbstractGo.js
@@ -1,11 +1,13 @@
-import { Link } from '@radix-ui/themes';
+import { Box, Link } from '@radix-ui/themes';
 import { getGoAddress } from '../../utils/PageAddressUtil';
 
 export default function AbstractGo({ link }) {
     const gotoLink = getGoAddress(link);
     return (
-        <Link href={gotoLink} size="2" mt="2" mb="3">
-            [阅读原文]
-        </Link>
+        <Box mt="2" mb="2">
+            <Link href={gotoLink} size="2">
+                [阅读原文]
+            </Link>
+        </Box>
     )
 }

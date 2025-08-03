8372eb769167c4d974c24f6faf7650f3f75cd451
Jane <Jane@larrys-MacBook-Pro.local>
2025-08-03 21:53:52 +0800

    monthly selected: subscription

---
+++ src/components/monthly-selected/Subscription.js
@@ -0,0 +1,77 @@
+import { Box, Flex, TextField, Button } from '@radix-ui/themes';
+import { Form } from '@radix-ui/react-form';
+import GlobalDialog from '../common/dialog/GlobalDialog';
+import { isEmailValid } from '../../utils/EmailUtil';
+import { useState } from 'react';
+import RequestUtil from '../../utils/APIRequestUtil';
+
+export default function Subscription() {
+    const [email, setEmail] = useState();
+    const [error, setError] = useState({ code: '', message: '' });
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+    const emailValidation = (email) => {
+        if (undefined === email || null == email || '' === email.trim()) {
+            return { code: 'subscription_params_invalid', message: '未输入有效内容，请返回重新输入！' };
+        }
+
+        if (!isEmailValid(email.trim())) {
+            return { code: 'subscription_params_invalid', message: '邮箱格式不正确，请返回修改！' };
+        }
+
+        return null;
+    }
+
+    const submitSubscription = async (email) => {
+        const resp = await RequestUtil.post('/api/subscriptions',
+            JSON.stringify({ 'email': email, 'type': 'MONTHLY_SELECTED' }),
+            { 'Content-Type': 'application/json' }
+        );
+
+        if (resp.status != 201) {
+            const respBody = await resp.json();
+            setError(respBody);
+        }
+
+        setDialogOpen(true);
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEmail(value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        var error = emailValidation(email);
+        if (null !== error) {
+            setError(error);
+            setDialogOpen(true);
+            return;
+        }
+
+        submitSubscription(email.trim());
+    }
+
+    return <>
+        <Box>
+            <Form>
+                <Flex gap="2" align="center" justify="between">
+                    <Box width="100%">
+                        <TextField.Root style={{ fontSize: '12px' }} name="email" placeholder="输入邮箱并订阅，月底将自动收到当月精选文章" id="email" value={email} onChange={handleChange} />
+                    </Box>
+                    <GlobalDialog
+                        title={'' != error.code ? '错误提示' : '提示'}
+                        titleColor={'' != error.code ? 'crimson' : ''}
+                        message={'' != error.code ? error.message : '您已成功订阅「每月精选」，您会在每个月底自动收到精选文章邮件！'}
+                        closeButtonName={'' != error.code ? '返回' : '关闭窗口'}
+                        dialogOpen={dialogOpen}
+                        setDialogOpen={setDialogOpen}
+                    />
+                    <Button onClick={handleSubmit}>订阅</Button>
+                </Flex>
+            </Form>
+        </Box>
+    </>
+}
--- src/pages/MonthlySelectedPage.js
+++ src/pages/MonthlySelectedPage.js
@@ -4,6 +4,7 @@ import CommonFooter from '../components/common/CommonFooter';
 import Meta from '../components/common/Meta';
 import MonthlySelectedCardList from '../components/monthly-selected/MonthlySelectedCardList';
 import { Box, Container, Flex } from '@radix-ui/themes';
+import Subscription from '../components/monthly-selected/Subscription';
 
 const meta = {
     title: '每月精选 - 博友圈 · 博客人的朋友圈！',
@@ -21,6 +22,7 @@ export default function MonthlySelectedPage() {
                     <Container size="2">
                         <Flex direction="column" gap="4">
                             <MainContentHeader content='首页文章更新的太快？几天不刷就感觉错过了什么？快来每月精选看看过去几个月的精选文章吧！' />
+                            <Subscription />
                             <MonthlySelectedCardList />
                         </Flex>
                     </Container>

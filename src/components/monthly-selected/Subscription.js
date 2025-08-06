2c846295f40ecf799ae223301313c9cab94def83
Jane <Jane@larrys-MacBook-Pro.local>
2025-08-07 07:25:52 +0800

    subscription

--- src/components/monthly-selected/Subscription.js
+++ src/components/monthly-selected/Subscription.js
@@ -53,10 +53,7 @@ export default function Subscription() {
             return;
         }
 
-        alert(email);
-
         submitSubscription(email.trim());
-        setEmail('');
     }
 
     return <>

4444b02a88a28e310b4774db53ac78fa54bf57ee
Jane <Jane@larrys-MacBook-Pro.local>
2025-08-12 10:26:04 +0800

    blog detail page refactoring

--- src/components/blog/BlogPosts.js
+++ src/components/blog/BlogPosts.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { formatDateStr } from '../../utils/DateUtil';
 import RequestUtil from '../../utils/APIRequestUtil';
-import { Card, Flex, Box, Text, Link } from '@radix-ui/themes';
+import { Card, Flex, Box, Text, Link, ScrollArea } from '@radix-ui/themes';
 import { getAbstractAddress, getGoAddress } from '../../utils/PageAddressUtil';
 
 export default function BlogPosts({ domain, blogStatusOk }) {
@@ -28,31 +28,28 @@ export default function BlogPosts({ domain, blogStatusOk }) {
             <Flex direction="column" gap="1">
                 <Text size="2" color="gray">收录文章</Text>
                 <Flex direction="column">
-                    <Box>
-                        {
-                            posts.map(
-                                (post, index) => (
-                                    <Flex gap="2" key={index} style={{ backgroundColor: index % 2 == 1 ? 'rgb(245, 245, 245)' : 'none' }}>
-                                        <Text size="2" color="gray">{formatDateStr(post.publishedAt, true)}</Text>
-                                        <Text size="2" style={{
-                                            display: '-webkit-box',
-                                            WebkitLineClamp: 1,
-                                            WebkitBoxOrient: 'vertical',
-                                            overflow: 'hidden'
-                                        }}>
-                                            {blogStatusOk ? <Link href={getGoAddress(post.link)}>{post.title}</Link>
-                                                : <Link href={getAbstractAddress(post.link)}>{post.title}</Link>}
-                                        </Text>
-                                    </Flex>
+                    <ScrollArea type="always" scrollbars="vertical" style={{ height: '280px' }}>
+                        <Box>
+                            {
+                                posts.map(
+                                    (post, index) => (
+                                        <Flex gap="2" key={index} style={{ backgroundColor: index % 2 == 1 ? 'rgb(245, 245, 245)' : 'none' }}>
+                                            <Text size="2" color="gray">{formatDateStr(post.publishedAt, true)}</Text>
+                                            <Text size="2" style={{
+                                                display: '-webkit-box',
+                                                WebkitLineClamp: 1,
+                                                WebkitBoxOrient: 'vertical',
+                                                overflow: 'hidden'
+                                            }}>
+                                                {blogStatusOk ? <Link href={getGoAddress(post.link)}>{post.title}</Link>
+                                                    : <Link href={getAbstractAddress(post.link)}>{post.title}</Link>}
+                                            </Text>
+                                        </Flex>
+                                    )
                                 )
-                            )
-                        }
-                    </Box>
-                    {
-                        showPostsLimit ? <Box>
-                            <Text size="1" color="crimson">* 仅显示最近 100 篇文章</Text>
-                        </Box> : ''
-                    }
+                            }
+                        </Box>
+                    </ScrollArea>
                 </Flex>
             </Flex>
         </Card>

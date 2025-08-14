96c2d1336f022fe0c7bb33781c94f04862718e33
Jane <Jane@larrys-MacBook-Pro.local>
2025-08-14 18:32:05 +0800

    footer

--- src/components/common/SiteFooter.js
+++ src/components/common/SiteFooter.js
@@ -1,9 +1,10 @@
-import { Box, Container, Flex, Link, ScrollArea, Separator, Text } from '@radix-ui/themes';
+import { Box, Container, Flex, Link, ScrollArea, Separator, Text, IconButton, DropdownMenu, Tooltip } from '@radix-ui/themes';
+import { Rss, Github, Cloud } from 'lucide-react';
 
 export default function SiteFooter() {
     return (
-        <Box mt="4">
-            <Container size="2">
+        <Container size="2">
+            <Box mt="4">
                 <Box>
                     <ScrollArea size="1" scrollbars="horizontal">
                         <Flex gap="2" justify="center" align="center">
@@ -19,25 +20,62 @@ export default function SiteFooter() {
                         </Flex>
                     </ScrollArea>
                 </Box>
-                <Box align="center">
-                    <Container size="1">
-                        <Separator size="4" my="2" />
-                    </Container>
+
+                <Separator size="4" my="2" />
+
+                <Box mb="2">
+                    <Flex gap="2" justify="center" align="center">
+                        <DropdownMenu.Root>
+                            <DropdownMenu.Trigger>
+                                <IconButton variant="soft">
+                                    <Tooltip content="本站文章 RSS 订阅">
+                                        <Rss width="14" height="14" />
+                                    </Tooltip>
+                                </IconButton>
+                            </DropdownMenu.Trigger>
+                            <DropdownMenu.Content>
+                                <Link size="1" href="https://www.boyouquan.com/feed.xml"><DropdownMenu.Item>推荐文章 RSS 订阅</DropdownMenu.Item></Link>
+                                <DropdownMenu.Separator />
+                                <Link size="1" href="https://www.boyouquan.com/feed.xml?sort=latest"><DropdownMenu.Item>最新文章 RSS 订阅</DropdownMenu.Item></Link>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Root>
+
+                        <Separator orientation="vertical" />
+
+                        <DropdownMenu.Root>
+                            <DropdownMenu.Trigger>
+                                <IconButton variant="soft">
+                                    <Tooltip content="本站代码 GitHub 开源">
+                                        <Github width="14" height="14" />
+                                    </Tooltip>
+                                </IconButton>
+                            </DropdownMenu.Trigger>
+                            <DropdownMenu.Content>
+                                <Link size="1" href="https://github.com/leileiluoluo/boyouquan-ui"><DropdownMenu.Item>前端代码 GitHub</DropdownMenu.Item></Link>
+                                <DropdownMenu.Separator />
+                                <Link size="1" href="https://github.com/leileiluoluo/boyouquan-api"><DropdownMenu.Item>后端代码 GitHub</DropdownMenu.Item></Link>
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Root>
+
+                        <Separator orientation="vertical" />
+
+                        <Link href="https://curl.qcloud.com/okTsvSrj">
+                            <IconButton variant="soft">
+                                <Tooltip content="本站使用的云服务">
+                                    <Cloud width="14" height="14" />
+                                </Tooltip>
+
+                            </IconButton>
+                        </Link>
+                    </Flex>
                 </Box>
-                <Flex direction="column" justify="center" align="center">
-                    <Box>
-                        <Text size="1">
-                            本站使用「<Link href="https://www.boyouquan.com/about#open-source">博友圈开源程序</Link>」创建，使用「<Link href="https://curl.qcloud.com/okTsvSrj">腾讯云</Link>」提供服务！
-                        </Text>
-                    </Box>
-                    <Box>
+                <Box>
+                    <Flex gap="2" direction="column" align="center">
                         <Link size="1" href="https://beian.miit.gov.cn/">辽ICP备2022012085号-2</Link>
-                    </Box>
-                    <Box>
                         <Text size="1">Copyright © 2023-2025 <Link href="https://www.boyouquan.com/home">博友圈</Link></Text>
-                    </Box>
-                </Flex>
-            </Container>
-        </Box>
+                    </Flex>
+                </Box>
+            </Box>
+        </Container>
     )
 }

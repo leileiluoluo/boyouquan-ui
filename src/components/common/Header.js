45fff9c11ccd470318180c24726f70779af40c6c
Jane <Jane@larrys-MacBook-Pro.local>
2025-08-22 20:24:29 +0800

    similar sites & header & about

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -35,7 +35,7 @@ export default function Header() {
                                         星球穿梭
                                     </a>
                                 </TabNav.Link>
-                                <TabNav.Link href="/blog-requests/add" active={pathname.startsWith("/blog-requests/add")}>提交博客</TabNav.Link>
+                                <TabNav.Link href="/blog-requests/add/email-validation" active={pathname.startsWith("/blog-requests/add")}>提交博客</TabNav.Link>
                                 <TabNav.Link href="/blog-requests" active={!pathname.startsWith("/blog-requests/add") && pathname.startsWith("/blog-requests")}>审核结果</TabNav.Link>
                             </TabNav.Root>
                         </ScrollArea>

ef5f82437c608a3b547d0b49194d9c39e2f25eda
Jane <Jane@larrys-MacBook-Pro.local>
2025-08-24 19:38:32 +0800

    header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -13,17 +13,22 @@ export default function Header() {
         <Box style={{ marginBottom: "10px" }}>
             <Container size="3">
                 <Flex align="center" justify="between">
-                    <Box mt="1" align="center">
+                    <Box mt="1" width="40%">
                         <Link href="/">
                             <img width="84px" height="30px" src="/assets/images/sites/logo/logo.svg"></img>
                         </Link>
                     </Box>
-                    <Box mt="-2">
+                    <Box mt="-2" width="60%">
                         <ScrollArea type="always" scrollbars="horizontal">
                             <TabNav.Root size="4" style={{ fontWeight: 'bold' }}>
                                 <TabNav.Link href="/home" active={pathname === "/home"}>首页</TabNav.Link>
                                 <TabNav.Link href="/monthly-selected" active={pathname === "/monthly-selected"}>每月精选</TabNav.Link>
                                 <TabNav.Link href="/blogs" active={pathname.startsWith("/blogs")}>博客广场</TabNav.Link>
+                                <TabNav.Link asChild>
+                                    <a href="/planet-shuttle" target="_blank" rel="noopener noreferrer">
+                                        星球穿梭
+                                    </a>
+                                </TabNav.Link>
                                 <TabNav.Link href="/blog-requests/add/email-validation" active={pathname.startsWith("/blog-requests/add")}>提交博客</TabNav.Link>
                                 <TabNav.Link href="/blog-requests" active={!pathname.startsWith("/blog-requests/add") && pathname.startsWith("/blog-requests")}>审核结果</TabNav.Link>
                             </TabNav.Root>

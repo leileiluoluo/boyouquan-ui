5ac83911cb6c4029bc7508ddf55bfb8c52df1678
Jane <Jane@larrys-MacBook-Pro.local>
2025-12-23 11:56:48 +0800

    annual report 2025

--- src/components/home/HomeBanner.tsx
+++ src/components/home/HomeBanner.tsx
@@ -4,7 +4,7 @@ export default function HomeBanner() {
     return (
         <Box mt="-5" mb="-4">
             <p>
-                <a href="/annual-reports"><img align="center" src="/assets/images/sites/banner/2025-report.jpg" /></a>
+                <a href="/annual-reports/2025"><img align="center" src="/assets/images/sites/banner/2025-report.jpg" /></a>
             </p>
         </Box>
     )

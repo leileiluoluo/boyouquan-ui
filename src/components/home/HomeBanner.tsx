611797203d1963f34244b578e922747fcc83ce88
Jane <Jane@larrys-MacBook-Pro.local>
2026-01-13 18:23:29 +0800

    home banner

--- src/components/home/HomeBanner.tsx
+++ src/components/home/HomeBanner.tsx
@@ -4,7 +4,7 @@ export default function HomeBanner() {
     return (
         <Box mt="-5" mb="-4">
             <p>
-                <a href="/annual-reports/2025"><img align="center" src="/assets/images/sites/banner/2025-report-now.jpg" /></a>
+                <a href="/moments"><img align="center" src="/assets/images/sites/banner/2026-happy-new-year.jpg" /></a>
             </p>
         </Box>
     )

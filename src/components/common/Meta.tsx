9310b9290e83be1a098fcc2830b5ae80be1f4b77
Jane <Jane@larrys-MacBook-Pro.local>
2026-04-07 19:29:07 +0800

    optimizing imports

--- src/components/common/Meta.tsx
+++ src/components/common/Meta.tsx
@@ -1,13 +1,14 @@
 import { Helmet } from 'react-helmet';
 
-export default function Meta({ meta }) {
-    if (null == meta) {
-        meta = {
-            title: '首页 - 博友圈 · 博客人的朋友圈！',
-            keywords: '博客, 博友, 博客圈, 博友圈, 朋友圈, 收录, RSS, 聚合, 中文, 独立博客',
-            description: '博友圈是博客人的专属朋友圈。我们的愿景是：将一个个散落在各处的孤岛连接成一片广袤无垠的新大陆！'
-        }
-    }
+import { MetaFields, MetaProps } from '@types';
+
+const defaultMetaFields: MetaFields = {
+    title: '首页 - 博友圈 · 博客人的朋友圈！',
+    keywords: '博客, 博友, 博客圈, 博友圈, 朋友圈, 收录, RSS, 聚合, 中文, 独立博客',
+    description: '博友圈是博客人的专属朋友圈。我们的愿景是：将一个个散落在各处的孤岛连接成一片广袤无垠的新大陆！'
+}
+
+export default function Meta({ meta = defaultMetaFields }: MetaProps) {
     return (
         <Helmet>
             <title>{meta.title}</title>

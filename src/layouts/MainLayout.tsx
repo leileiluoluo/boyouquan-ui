1626df109dc0627d2e6c1d23359da3442bee9fa3
Jane <Jane@larrys-MacBook-Pro.local>
2026-04-10 08:22:25 +0800

    footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,11 +9,12 @@ import MainFooter from '@layouts/footer/MainFooter';
 const { Content } = Layout;
 
 // 不需要 Header/Footer 的路径列表
-const NO_LAYOUT_PATHS = ['/planet-shuttle'];
+const NO_LAYOUT_PATHS = ['/planet-shuttle', '/go', '/certificates'];
 
 const MainLayout: React.FC = () => {
     const location = useLocation();
-    const shouldShowLayout = !NO_LAYOUT_PATHS.includes(location.pathname);
+    
+    const shouldShowLayout = !NO_LAYOUT_PATHS.some(path => location.pathname.includes(path));
 
     if (!shouldShowLayout) {
         return <Outlet />;
@@ -22,12 +23,12 @@ const MainLayout: React.FC = () => {
     return (
         <Layout>
             <MainHeader />
-            <Content style={{ margin: '16px 0', padding: '0 48px' }}>
+            <Content>
                 <div
                     style={{
                         backgroundColor: 'white',
                         minHeight: 400,
-                        padding: 24,
+                        padding: 20,
                     }}
                 >
                     <Outlet />

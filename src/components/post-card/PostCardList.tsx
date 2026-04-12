c4a0fb642b215efab691e2f1dec056b81fcf806e
Jane <Jane@larrys-MacBook-Pro.local>
2026-04-12 08:37:37 +0800

    home page

--- src/components/post-card/PostCardList.tsx
+++ src/components/post-card/PostCardList.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { Flex, Card, Skeleton, Typography, Empty } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Flex, Card } from 'antd';
 import { scrollToHash, clearHash } from '../../utils/ScrollUtil';
 import PostCard from './PostCard';
 import Pagination from '../pagination/Pagination';
 import RequestUtil from '../../utils/APIRequestUtil';
+import SinglePostCard from './SinglePostCard';
 
-const { Text } = Typography;
+interface PostCardListProps {
+    sort: string,
+    keyword: string,
+    showPinned: boolean
+}
 
-export default function PostCardList({ sort, keyword, showPinned }) {
+const PostCardList: React.FC<PostCardListProps> = ({ sort, keyword, showPinned }: PostCardListProps) => {
     const [pageNo, setPageNo] = useState(1);
     const [pageSize, setPageSize] = useState(0);
     const [total, setTotal] = useState(0);
@@ -37,72 +41,12 @@ export default function PostCardList({ sort, keyword, showPinned }) {
         document.getElementById('switch-sort-type').scrollIntoView();
     }
 
-    if (!dataReady) {
-        return (
-            <Flex vertical gap={12}>
-                <Flex vertical gap={8}>
-                    {Array.from({ length: 10 }).map((_, i) => (
-                        <Card key={i} style={{ width: '100%' }}>
-                            <Flex vertical gap={4}>
-                                <Skeleton.Input active size="small" style={{ width: 240 }} />
-                                <Skeleton.Input active size="default" style={{ width: '100%' }} />
-                                <Flex gap={4} align="center">
-                                    <Skeleton.Avatar active size="small" />
-                                    <Skeleton.Input active size="small" style={{ width: 80 }} />
-                                    <Skeleton.Input active size="small" style={{ width: 50 }} />
-                                    <Skeleton.Input active size="small" style={{ width: 50 }} />
-                                    <Skeleton.Button active size="small" shape="circle" />
-                                </Flex>
-                            </Flex>
-                        </Card>
-                    ))}
-                </Flex>
-
-                <Pagination
-                    pageNo={pageNo}
-                    pageSize={pageSize}
-                    total={total}
-                    setCurrectPage={setCurrectPage} />
-            </Flex>
-        );
-    }
-
-    if (null !== keyword && '' !== keyword && 0 === total) {
-        return (
-            <Flex vertical>
-                <Empty
-                    description="未找到相关文章，试试更换关键词吧！"
-                    style={{ marginTop: 40, marginBottom: 40 }}
-                />
-                <Pagination
-                    pageNo={pageNo}
-                    pageSize={pageSize}
-                    total={total}
-                    setCurrectPage={setCurrectPage} />
-            </Flex>
-        );
-    }
-
     return (
         <Flex vertical gap={12}>
             <Flex vertical gap={8}>
                 {posts.map(
                     (post, index) => (
-                        <Card key={index} style={{ width: '100%' }}>
-                            <PostCard
-                                key={index}
-                                showPinned={showPinned}
-                                pinned={post.pinned}
-                                blogDomainName={post.blogDomainName}
-                                blogName={post.blogName}
-                                blogStatusOk={post.blogStatusOk}
-                                blogAdminMediumImageURL={post.blogAdminMediumImageURL}
-                                link={post.link}
-                                title={post.title}
-                                description={post.description}
-                                publishedAt={post.publishedAt}
-                                linkAccessCount={post.linkAccessCount} />
-                        </Card>
+                        <SinglePostCard post={post}/>
                     )
                 )}
             </Flex>
@@ -113,5 +57,7 @@ export default function PostCardList({ sort, keyword, showPinned }) {
                 total={total}
                 setCurrectPage={setCurrectPage} />
         </Flex>
-    )
-}
+    );
+};
+
+export default PostCardList;
